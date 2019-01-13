f700933539c55dd3ca5246c99083e5140a847769
Kchar <38297595+kevinlichang@users.noreply.github.com>
2019-01-12 19:09:04 -0500

    added validators to php

--- contact.php
+++ contact.php
@@ -3,32 +3,33 @@
 $nameErr = $emailErr = "";
 $name = $email = $message = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
+$email_to = "kevinlichangportfolio@gmail.com";
+$email_subject = "Message from Portfolio Contact";
 
-  if (empty($_POST["message"])) {
-    $message = "";
-  } else {
-    $message = test_input($_POST["message"]);
+if (empty($_POST["name"])) {
+  $nameErr = "Name is required";
+} else {
+  $name = test_input($_POST["name"]);
+  // check if name only contains letters and whitespace
+  if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+    $nameErr = "Only letters and white space allowed";
   }
+}
+
+if (empty($_POST["email"])) {
+  $emailErr = "Email is required";
+} else {
+  $email_from = test_input($_POST["email"]);
+  // check if e-mail address is well-formed
+  if (!filter_var($email_from, FILTER_VALIDATE_EMAIL)) {
+    $emailErr = "Invalid email format";
+  }
+}
+
+if (empty($_POST["message"])) {
+  $message = "";
+} else {
+  $message = test_input($_POST["message"]);
 }
 
 function test_input($data) {
@@ -37,4 +38,9 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($email_to, $email_subject, $message, $headers)
 ?>
